8a24fad8d93f4c72f26643691103bad49a9bac48
philgrayphilgray <you@example.comclear>
2018-02-22 08:23:09 -0500

    Enclose last test in it block to resolve failing test and refactor.

--- test/features/user-visits-single-item-test.js
+++ test/features/user-visits-single-item-test.js
@@ -1,41 +1,34 @@
 const { assert } = require("chai");
 const { buildItemObject } = require("../test-utils");
 /*
- * Test Item Constants 
- */
-
-// const imageUrl =
-//   "https://upload.wikimedia.org/wikipedia/commons/d/df/Laelaps-cope.jpg";
-// const title = "Dryptosaurus";
-// const description =
-//   "Dryptosaurus was a large, bipedal, ground-dwelling carnivore, that could grow up to 7.5 m (24.6 ft) long.";
-
-/*
  * Tests 
  */
 
 describe("when user submits a new item", () => {
+  //setup:constants
   const { title, description, imageUrl } = buildItemObject();
+
   beforeEach(() => {
-    //setup
+    //setup:create new item
     browser.url("/items/create");
     browser.setValue("#title-input", title);
     browser.setValue("#description-input", description);
     browser.setValue("#imageUrl-input", imageUrl);
-
-    // exercise
     browser.click("#submit-button");
   });
 
   it("displays the new item on the `root` page", () => {
-    //verify
+    //verify: container text includes item title
     assert.include(browser.getText("#items-container"), title);
   });
-  describe("and clicks on the new item", () => {
-    // exercise
-    browser.click(".item-card a");
 
-    // verify
-    assert.include(browser.getText("#item-description"), description);
+  describe("and clicks on the new item", () => {
+    it("displays the new item description", () => {
+      // exercise: click on item link
+      browser.click(".item-card a");
+
+      // verify: item-description block on item page contains new item description
+      assert.include(browser.getText("#item-description"), description);
+    });
   });
 });
