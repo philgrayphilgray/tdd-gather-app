859bf87e62121b4ff448b8d1b4d82806cb9e535a
philgrayphilgray <thephilgray@gmail.com>
2018-02-23 17:39:32 -0500

    Test to validate bad data and display error messages.

--- test/routes/update-test.js
+++ test/routes/update-test.js
@@ -39,20 +39,55 @@ describe("Server path: /items/:id/update", () => {
     });
   });
   describe("POST", () => {
-    it("updates the record for the item in the database", async () => {
-      let { title, description, imageUrl, _id } = await seedItemToDatabase();
-      title = "New Title";
-      description = "New description";
-      const response = await request(app)
-        .post("/items/" + _id + "/update")
-        .type("form")
-        .send({ title, description, imageUrl });
-      const updatedItem = await Item.findOne({
-        title,
-        description,
-        imageUrl
+    describe("when a user changes the `title` field and clicks to update", () => {
+      it("updates the record for the item in the database", async () => {
+        let { title, description, imageUrl, _id } = await seedItemToDatabase();
+        title = "New Title";
+        description = "New description";
+        const response = await request(app)
+          .post("/items/" + _id + "/update")
+          .type("form")
+          .send({ title, description, imageUrl });
+        const updatedItem = await Item.findOne({
+          title,
+          description,
+          imageUrl
+        });
+        assert.ok(updatedItem, "Item was not updated in the db");
       });
-      assert.ok(updatedItem, "Item was not updated in the db");
+      it("redirects to `/` after updating the item in the db", async () => {
+        let { title, description, imageUrl, _id } = await seedItemToDatabase();
+        title = "New Title";
+        description = "New description";
+        const response = await request(app)
+          .post("/items/" + _id + "/update")
+          .type("form")
+          .send({ title, description, imageUrl });
+
+        assert.equal(response.status, 302);
+        assert.equal(response.headers.location, "/");
+      });
+    });
+    describe("when a user attempts to update an item with an invalid value", () => {
+      it("displays an error message and the changes are not saved to the db", async () => {
+        let { title, description, imageUrl, _id } = await seedItemToDatabase();
+        title = "";
+        const response = await request(app)
+          .post("/items/" + _id + "/update")
+          .type("form")
+          .send({ title, description, imageUrl });
+        const updatedItem = await Item.findOne({
+          title,
+          description,
+          imageUrl
+        });
+        assert.notOk(updatedItem);
+        assert.equal(response.status, 400);
+        assert.include(
+          parseTextFromHTML(response.text, ".input-form"),
+          "required"
+        );
+      });
     });
   });
 });
