43b9aeb650bfecdd8a10d410d88615adc30751f2
philgrayphilgray <thephilgray@gmail.com>
2018-02-23 17:40:17 -0500

    Implement error handling on update POST.

--- routes/index.js
+++ routes/index.js
@@ -44,23 +44,39 @@ router.get("/items/:itemId/update", async (req, res, next) => {
 });
 
 router.post("/items/:itemId/update", async (req, res, next) => {
+  const selectedItem = await Item.findById(req.params.itemId);
   const { title, description, imageUrl } = req.body;
-  const updatedItem = await Item.findByIdAndUpdate(req.params.itemId, {
+  const newItem = new Item({
+    ...selectedItem,
     title,
     description,
-    imageUrl
+    imageUrl,
+    _id: req.params.itemId
   });
-  updatedItem.validateSync();
-  if (updatedItem.errors) {
-    res.status(400).render("update", {
-      title,
-      description,
-      imageUrl
-    });
+  newItem.validateSync();
+  if (newItem.errors) {
+    res.status(400).render("update", { selectedItem: newItem });
   } else {
-    await updatedItem.save();
-    res.redirect("/");
-    res.status = 302;
+    // To turn on update validators, set the runValidators option.
+    const updateOptions = { runValidators: true };
+    try {
+      const updatedItem = await Item.findByIdAndUpdate(
+        req.params.itemId,
+        {
+          title,
+          description,
+          imageUrl
+        },
+        updateOptions
+      );
+
+      updatedItem.validateSync();
+
+      res.redirect("/");
+      res.status = 302;
+    } catch (error) {
+      res.status(400).render("update", { selectedItem });
+    }
   }
 });
 
